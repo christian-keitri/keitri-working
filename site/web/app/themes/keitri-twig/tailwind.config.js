543fb9d2ea7e58aa67843fcf40a5b81ab1580365
BOB <christian.salapate@keitri.com>
2025-06-17 13:25:32 +0800

    first design and animation

--- site/web/app/themes/keitri-twig/tailwind.config.js
+++ site/web/app/themes/keitri-twig/tailwind.config.js
@@ -5,6 +5,25 @@ module.exports = {
     "./templates/components/**/*.{twig,php}",
     "./*.php"
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      keyframes: {
+        slide: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
+        'slide-and-color': {
+          '0%': { transform: 'translateX(-50%)', backgroundColor: '#22d3ee' }, // cyan-400
+          '100%': { transform: 'translateX(50%)', backgroundColor: '#fbbf24' }, // amber-400
+        },
+      },
+      animation: {
+        slide: 'slide 3s linear infinite',
+        'slide-fast': 'slide 0.5s ease-out forwards',
+        'slide-and-color': 'slide-and-color 3s ease-in-out infinite alternate',
+        'slow-bounce': 'bounce 2s infinite',
+      },
+    },
+  },
   plugins: [],
 }
